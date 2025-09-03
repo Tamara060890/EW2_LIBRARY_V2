4795746e79b1127f81bf8e0128d33185c9f805f9
liesjedebroey <annelies.debroey@gmail.com>
2025-09-03 15:41:59 +0200

    Main menu (final changes must be made: translations + search by author method)

--- src/main/java/repository/BookRepositoryImpl.java
+++ src/main/java/repository/BookRepositoryImpl.java
@@ -3,21 +3,21 @@ package repository;
 //REPOSITORY (book): This is the actual logic to save books in a list, database or file.
 
 import model.Book;
-import model.BookType;
-import service.BookService;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class BookRepositoryImpl implements BookRepository {
     private final Map<String, Book> bookInventory = new HashMap<>();
 
+    /*private int counter = 1;
 
+    private String generateIntecID() {
+        return String.format("INTEC%03d", counter++);
+    }*/
 
     @Override
     public void addBook(Book book) {
@@ -26,42 +26,52 @@ public class BookRepositoryImpl implements BookRepository {
 
     @Override
     public boolean deleteBook(String intecID) {
-        return false;
+        return bookInventory.remove(intecID) != null;
     }
 
     @Override
-    public boolean editBook(String intecID, Book updatedBook) {
-        return false;
+    public void updateBook(Book updatedBook) {
+            bookInventory.put(updatedBook.getIntecID(), updatedBook);
     }
 
     @Override
     public Book searchBookTitle(String title) {
-        return null;
+        return bookInventory.values().stream()
+                .filter(book -> book.getTitle().equalsIgnoreCase(title))
+                .findFirst()
+                .orElse(null);
     }
 
+
     @Override
-    public Book searchBookAuthor(String author) {
-        return null;
+    public List<Book> searchBookAuthor(String author) {
+        return bookInventory.values().stream()
+                .filter(book -> book.getAuthor().equalsIgnoreCase(author))
+                .collect(Collectors.toList());
     }
+
 
     @Override
     public Book searchBookISBN(String isbn) {
-        return null;
+        return bookInventory.values().stream()
+                .filter(book -> book.getIsbn().equalsIgnoreCase(isbn))
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public Book searchBookIntecID(String intecID) {
-        return null;
+        return bookInventory.get(intecID);
+    }
+
+    @Override
+    public void loadBooksFromFile(String fileName) {
+        throw new UnsupportedOperationException("CSV import is managed in Business Logic Class: BookService.");
     }
 
     @Override
     public List<Book> getAllBooks() {
         return new ArrayList<>(bookInventory.values());
-    }
-
-    @Override
-    public void loadBooksFromFile(String fileName) {
-
     }
 
 }
